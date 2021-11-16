c4d78cbff538967ac1c7d0a095e56ef6a0858e57
fulgence boka <fulgenceboka@gmail.com>
2021-11-16 00:11:07 +0000

    indentation

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,17 +1,16 @@
-let xmark = document.querySelector('.fa-xmark');
-let bars = document.querySelector('.fa-bars');
-let navbarList = document.querySelector('.navbar__list');
-xmark.addEventListener('click', isHiddenToggle );
-bars.addEventListener('click', isHiddenToggle );
+let xmark = document.querySelector(".fa-xmark");
+let bars = document.querySelector(".fa-bars");
+let navbarList = document.querySelector(".navbar__list");
+xmark.addEventListener("click", isHiddenToggle);
+bars.addEventListener("click", isHiddenToggle);
 
-function isHiddenToggle (e) {
-	bars.classList.toggle('is-hidden');
-	xmark.classList.toggle('is-hidden');
+function isHiddenToggle(e) {
+  bars.classList.toggle("is-hidden");
+  xmark.classList.toggle("is-hidden");
 
-	if(e.path[0].classList.contains('fa-bars')){
-		navbarList.style.display = 'flex';
-	}else{
-		navbarList.style.display = 'none';
-	}
-
+  if (e.path[0].classList.contains("fa-bars")) {
+    navbarList.style.display = "flex";
+  } else {
+    navbarList.style.display = "none";
+  }
 }
