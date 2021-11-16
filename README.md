887741cd7ec2026d4cad0918be3b92fcb3c95f9f
fulgence boka <fulgenceboka@gmail.com>
2021-11-16 00:00:17 +0000

    Creation du mobile-screen partie 1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # teamboka
 Un centre de formation au métier du développement web 
+
+## Creation de la navigation
+
--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,5 +1,17 @@
-let div = document.createElement('div');
-div.setAttribute('class', 'big');
-div.innerText = "je suis big";
+let xmark = document.querySelector('.fa-xmark');
+let bars = document.querySelector('.fa-bars');
+let navbarList = document.querySelector('.navbar__list');
+xmark.addEventListener('click', isHiddenToggle );
+bars.addEventListener('click', isHiddenToggle );
 
-document.querySelector('.main').appendChild(div);
+function isHiddenToggle (e) {
+	bars.classList.toggle('is-hidden');
+	xmark.classList.toggle('is-hidden');
+
+	if(e.path[0].classList.contains('fa-bars')){
+		navbarList.style.display = 'flex';
+	}else{
+		navbarList.style.display = 'none';
+	}
+
+}
